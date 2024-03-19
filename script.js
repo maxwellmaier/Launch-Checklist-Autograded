e62947eb11d413ba705baeab3a01546555b82985
maxwell.maier <maxwellmaier89@gmail.com>
2024-03-18 20:36:35 -0500

    fixed invalid entry

--- script.js
+++ script.js
@@ -40,22 +40,21 @@ window.addEventListener("load", function() {
     let listedPlanetsResponse = myFetch()
     listedPlanetsResponse.then(function(result) {
       listedPlanets = result;
-      console.log(listedPlanets);
     }).then(function() {
-      console.log(listedPlanets);
       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
       const planet = pickPlanet(listedPlanets)
       addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image)
     })
   
-    const pilot = document.querySelector('input[name="pilotName"]').value
-    const copilot = document.querySelector('input[name="copilotName"]').value
-    const fuelLevel = document.querySelector('input[name="fuelLevel"]').value
-    const cargoMass = document.querySelector('input[name="cargoMass"]').value
-    const list = document.querySelector("#faultyItems");
     const form = document.querySelector('form')
   
     form.addEventListener('submit', function(event) {
+      const pilot = document.querySelector('input[name="pilotName"]').value
+      const copilot = document.querySelector('input[name="copilotName"]').value
+      const fuelLevel = document.querySelector('input[name="fuelLevel"]').value
+      const cargoMass = document.querySelector('input[name="cargoMass"]').value
+      const list = document.querySelector("#faultyItems");
+  
       formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass);
       event.preventDefault();
     })
